6db9cdd7ee832b120340370945f230c685406a93
Yong Social <yongcomics@gmail.com>
2026-06-28 03:12:08 +0100

    Update offers.js

--- offers.js
+++ offers.js
@@ -1,6 +1,9 @@
 const offers = [
     "https://changelly.com/?ref_id=7mdOLkhFXpkzMSNX",
+    "https://omg10.com/4/10470895",
     "https://changelly.com/buy?from=usd&to=btc&amount=150&ref_id=7mdOLkhFXpkzMSNX",
     "https://earnne.com/",
+    "https://omg10.com/4/10242701",
+    "https://omg10.com/4/10242827",
     "https://ref-earn.com/QSqDX8"
 ];
