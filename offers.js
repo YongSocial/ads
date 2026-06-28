cc606a4fcaa0d2015e2165469356257555012877
Yong Social <yongcomics@gmail.com>
2026-06-28 02:55:19 +0100

    Create offers.js

---
+++ offers.js
@@ -0,0 +1,6 @@
+const offers = [
+    "https://changelly.com/?ref_id=7mdOLkhFXpkzMSNX",
+    "https://changelly.com/buy?from=usd&to=btc&amount=150&ref_id=7mdOLkhFXpkzMSNX",
+    "https://earnne.com/",
+    "https://ref-earn.com/QSqDX8"
+];
